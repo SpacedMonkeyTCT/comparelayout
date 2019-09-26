e984e299ca07253a8fd144540415cb9112a59384
Ben Ramsay <spacedmonkeytct@gmail.com>
2019-09-26 08:58:13 +0100

    New layout

---
+++ layout5/cmd/layout2/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/SpacedMonkeyTCT/comparelayout/layout5/internal/application"
+	"github.com/SpacedMonkeyTCT/comparelayout/layout5/pkg/component"
+	"github.com/SpacedMonkeyTCT/comparelayout/layout5/pkg/component2"
+)
+
+func main() {
+	c1 := component.New()
+	c2 := component2.New()
+	m := application.NewModule(c1, c2)
+	m.Action()
+}
